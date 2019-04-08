e037a54b4e3cec893e904efef2f507abcf0a4b16
[Dan Farrow] <dan@zellement.com>
2019-04-08 22:20:31 +0100

    Spotify player amend

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -4,8 +4,7 @@ const SpotifyPlayer = class extends React.Component {
 
 	render() {
 
-
-		// This currently replaces the content with the Spotify iframe
+		// Function called below by onClick, then replaces content with iframe
 		function spotifyClick () {
 			document.getElementById("spotifyPlaceholder").innerHTML = '<iframe title="Shantymen Spotify Player" src="https://open.spotify.com/embed/artist/06bi895n8wIOUi35sk65gJ" width="100%" height="400" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
 		}
