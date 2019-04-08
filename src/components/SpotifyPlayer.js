e50b12ac8dd852d20f6942d0e51f9e1d891bb47d
[Dan Farrow] <dan@zellement.com>
2019-04-08 21:15:10 +0100

    Load Spotify on demand

--- src/components/SpotifyPlayer.js
+++ src/components/SpotifyPlayer.js
@@ -1,7 +1,31 @@
 import React from "react"
 
-const SpotifyPlayer = () => (
-    <iframe title="Shantymen Spotify Player" src="https://open.spotify.com/embed/artist/06bi895n8wIOUi35sk65gJ" width="100%" height="400" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-)
+const SpotifyPlayer = class extends React.Component {
+
+	render() {
+
+		// https://stackoverflow.com/questions/26107125/cannot-read-property-addeventlistener-of-null/30063375
+		// Set a variable for "el" to check event listener is not null
+		var el = document.getElementById('spotifyPlaceholder');
+		// If not null, do this
+		if(el){
+			el.addEventListener("click", spotifyClick);
+		}
+
+		// This currently replaces the content with the Spotify iframe
+		function spotifyClick () {
+			document.getElementById("spotifyPlaceholder").innerHTML = '<iframe title="Shantymen Spotify Player" src="https://open.spotify.com/embed/artist/06bi895n8wIOUi35sk65gJ" width="100%" height="400" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>';
+		}
+	
+		return(
+			<div>
+				<div id="spotifyPlaceholder">
+					Show me Spotify player
+				</div>
+			</div>
+		)
+	}
+}
+
 
 export default SpotifyPlayer
