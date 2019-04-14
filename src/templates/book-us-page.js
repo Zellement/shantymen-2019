b4efac27ee11d36d548490d414ecbaa6d6c77260
[Dan Farrow] <dan@zellement.com>
2019-04-14 13:18:34 +0100

    Form updated

--- src/templates/book-us-page.js
+++ src/templates/book-us-page.js
@@ -17,14 +17,15 @@ function BookUsPage({ data }) {
 
         <p>If you would like to enquire about booking us to play, please fill out the form below and we will get back to you as soon as possible.</p>
          
-          <form name="bookUs" method="POST" data-netlify="true">
-            <label>Your Name: <input type="text" name="name" /></label>   
-            <label>Your Email: <input type="email" name="email" /></label>
-            <label>Your Telephone: <input type="text" name="telephone" /></label>
-            <label>Venue: <input type="text" name="venue" /></label>
-            <label>Date &amp; time requested: <input type="text" name="datetime" /></label>
-            <label>Details: <textarea name="details"></textarea></label>
-            <button type="submit">Send</button>
+          <form className="form" name="bookUs" method="POST" data-netlify="true">
+            <label className="hidden">Don't fill this out if you're human: <input name="bot-field" /></label>
+            <label>Your Name: <input placeholder="Enter your name" type="text" name="name" /></label>   
+            <label>Your Email: <input placeholder="Enter your email"  type="email" name="email" /></label>
+            <label>Your Telephone: <input placeholder="Enter your telephone"  type="text" name="telephone" /></label>
+            <label>Venue: <input placeholder="Enter your venue"  type="text" name="venue" /></label>
+            <label>Date &amp; time requested: <input placeholder="Enter your date and time"  type="text" name="datetime" /></label>
+            <label>Details: <textarea placeholder="Enter any additional information" rows="5"  name="details"></textarea></label>
+            <button className="btn btn--orange" type="submit">Send</button>
           </form>
 
         </div>
