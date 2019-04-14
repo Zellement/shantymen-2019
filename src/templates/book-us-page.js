7b22e1390c575816c7874b11843132d2de4a2dea
[Dan Farrow] <dan@zellement.com>
2019-04-14 15:07:53 +0100

    Form as component

--- src/templates/book-us-page.js
+++ src/templates/book-us-page.js
@@ -4,90 +4,20 @@ import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
 import FullImageBg from '../components/FullImageBg'
-import { navigateTo } from "gatsby-link";
+import ContactForm from '../components/ContactForm'
 
-function encode(data) {
-  return Object.keys(data)
-    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-    .join("&");
-}
 
-export default class Contact extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    const form = e.target;
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({
-        "form-name": form.getAttribute("name"),
-        ...this.state
-      })
-    })
-      .then(() => navigateTo(form.getAttribute("action")))
-      .catch(error => alert(error));
-  };
-
-  render() {
-    return (
+const BookUsPage = ({data}) => (
     <div className="wrapper">
+      <FullImageBg bgImage={data.allFile.edges[0].node.childImageSharp.fluid} className="home-page" />
       <Header />
       <Hero h1="Book Us" h2="Drop us a message to enquire about a booking" />
       <div className="main-content">
         <div className="main-content__copy">
 
         <p>If you would like to enquire about booking us to play, please fill out the form below and we will get back to you as soon as possible.</p>
-
-        <h1>Contact</h1>
-        <form
-          name="contact"
-          method="post"
-          action="/thanks/"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type="hidden" name="form-name" value="contact" />
-          <p hidden>
-            <label>
-              Don’t fill this out:{" "}
-              <input name="bot-field" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your name:<br />
-              <input type="text" name="name" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Your email:<br />
-              <input type="email" name="email" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message:<br />
-              <textarea name="message" onChange={this.handleChange} />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
          
-
+          <ContactForm />
 
         </div>
         <div className="main-content__aside">
@@ -95,9 +25,9 @@ export default class Contact extends React.Component {
       </div>
       <Footer />
     </div>
-    );
-  }
-}
+)
+
+export default BookUsPage
 
 export const BookUsPageQuery = graphql`
 {
