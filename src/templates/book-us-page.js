48c9020a8c21dd5023f26328c7c01a594a53a8f4
[Dan Farrow] <dan@zellement.com>
2019-04-14 13:10:45 +0100

    Add new form

--- src/templates/book-us-page.js
+++ src/templates/book-us-page.js
@@ -14,7 +14,19 @@ function BookUsPage({ data }) {
       <Hero h1="Book Us" h2="Drop us a message to enquire about a booking" />
       <div className="main-content">
         <div className="main-content__copy">
-         stuff
+
+        <p>If you would like to enquire about booking us to play, please fill out the form below and we will get back to you as soon as possible.</p>
+         
+          <form name="bookUs" method="POST" data-netlify="true">
+            <label>Your Name: <input type="text" name="name" /></label>   
+            <label>Your Email: <input type="email" name="email" /></label>
+            <label>Your Telephone: <input type="text" name="telephone" /></label>
+            <label>Venue: <input type="text" name="venue" /></label>
+            <label>Date &amp; time requested: <input type="text" name="datetime" /></label>
+            <label>Details: <textarea name="details"></textarea></label>
+            <button type="submit">Send</button>
+          </form>
+
         </div>
         <div className="main-content__aside">
         </div>
