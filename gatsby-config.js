fd1354273b42b145efd071c36dddfe5890bf4398
[Dan Farrow] <dan@zellement.com>
2019-04-23 21:49:10 +0100

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,21 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-21853933-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         path: `${__dirname}/src/gigs`,
