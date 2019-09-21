54d6f0befe44c15fb60c807983fbada818c14d86
Dan Farrow <dan@zellement.com>
2019-09-21 20:00:13 +0100

    Add in day

--- src/components/gigs/GigsAll.js
+++ src/components/gigs/GigsAll.js
@@ -34,11 +34,19 @@ const GigsAll = () => (
           <div className="gigs-all">
             <h2>Gig Listings</h2>
             {data.allMarkdownRemark.edges.map(gigdata => (
-              <div className="gigs-all__gig" key={gigdata.node.frontmatter.location + gigdata.node.frontmatter.datetime}>
+
+              <div key={gigdata.node.frontmatter.location + gigdata.node.frontmatter.datetime + gigdata.node.frontmatter.location + gigdata.node.frontmatter.datetime}>
+
+              {
+                checkPassedGig(gigdata.node.frontmatter.datetime)
+              }
+
+              <div className="gigs-all__gig">
                 <div className="gigs-all__card">
                   <div className="gigs-all__card-content">
                     <GiCalendar className="gigs-all__icon" />
-                    <h3 className="gigs-all__datetime" key={gigdata.node.frontmatter.datetime}>
+                    <h3
+                      className="gigs-all__datetime">
                       { timeConverter(gigdata.node.frontmatter.datetime) }
                     </h3>
                   </div>
@@ -46,10 +54,12 @@ const GigsAll = () => (
                 <div className="gigs-all__card gigs-all__card-orange">
                   <div className="gigs-all__card-content">
                     <GiPositionMarker className="gigs-all__icon" />
-                    <h3 className="gigs-all__location" key={gigdata.node.frontmatter.location}> {gigdata.node.frontmatter.location}</h3>
+                    <h3
+                      className="gigs-all__location"> {gigdata.node.frontmatter.location}</h3>
                   </div>
                 </div>
-                <HTMLContent className="gigs-all__content" key={gigdata.node.html} content={gigdata.node.html} />
+                <HTMLContent className="gigs-all__content" content={gigdata.node.html} />
+              </div>
               </div>
             ))}
           </div>
@@ -61,16 +71,27 @@ const GigsAll = () => (
 function timeConverter(unixTimestamp){
   var a = new Date(unixTimestamp * 1000);
   var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
   var year = a.getFullYear();
   var month = months[a.getMonth()];
   var date = a.getDate();
+  var day = days[a.getDay()];
   var hour = a.getHours();
   var min = a.getMinutes();
   if (min === 0) {
     min = '00';
   }
-  var time = date + ' ' + month + ' ' + year + ' @ ' + hour + ':' + min;
+  var time = day + ", " + date + ' ' + month + ' ' + year + ' @ ' + hour + ':' + min;
   return time;
+}
+
+function checkPassedGig(dateGig){
+  const dateNow = (Date.now()/1000);
+  //console.log("The date now is " + dateNow + ". The date of this gig is " + dateGig)
+  if (dateNow < dateGig) {
+    //console.log("Hello")
+    const gigHasPassed = true;
+  }
 }
 
 export default GigsAll
