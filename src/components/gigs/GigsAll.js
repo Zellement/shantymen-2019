497ddfe1ed89dc698c8773341c631709f8ee6909
Dan Farrow <dan@zellement.com>
2019-09-21 13:49:02 +0100

    Update config

--- src/components/gigs/GigsAll.js
+++ src/components/gigs/GigsAll.js
@@ -3,14 +3,17 @@ import { StaticQuery, graphql } from "gatsby"
 import { HTMLContent } from '../../components/Content'
 import { GiPositionMarker, GiCalendar } from 'react-icons/gi'
 
+
+
 const GigsAll = () => (
+
 	<StaticQuery
 	    query={graphql`
         query 
         {
           allMarkdownRemark(
           sort: {
-            fields: [frontmatter___date]
+            fields: [frontmatter___datetime]
             order: ASC
           }
             filter: {frontmatter: {templateKey: {eq: "gig-listing"}}}
@@ -20,7 +23,6 @@ const GigsAll = () => (
                 html
                 frontmatter {
                   location
-                  date
                   datetime
                 }
               }
@@ -28,8 +30,10 @@ const GigsAll = () => (
           }
         }
 	    `}
+
 	    render={data => (
         <div>
+
           <div className="gigs-all">
             <h2>Gig Listings</h2>
             {data.allMarkdownRemark.edges.map(gigdata => (
@@ -37,7 +41,19 @@ const GigsAll = () => (
                 <div className="gigs-all__card">
                   <div className="gigs-all__card-content">
                     <GiCalendar className="gigs-all__icon" />
-                    <h3 className="gigs-all__datetime" key={gigdata.node.frontmatter.datetime}>{gigdata.node.frontmatter.datetime}</h3>
+                    <h3 className="gigs-all__datetime" key={gigdata.node.frontmatter.datetime}>
+                    {//gigdata.node.frontmatter.datetime
+                    }
+
+
+
+                    {
+                      //const unixDate = gigdata.node.frontmatter.date;
+                      timeConverter(gigdata.node.frontmatter.datetime)
+                    }
+
+                    </h3>
+
                   </div>
                 </div>
                 <div className="gigs-all__card gigs-all__card-orange">
@@ -54,5 +70,21 @@ const GigsAll = () => (
 	    )}
 	  />
 )
+
+
+
+function timeConverter(unixTimestamp){
+  var a = new Date(unixTimestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+  return time;
+  //console.log(time);
+}
+//console.log(timeConverter(0));
 
 export default GigsAll
